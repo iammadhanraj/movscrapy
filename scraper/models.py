a592ad89faca719b35c9cdd1609437b5cc065a19
iammadhanraj <madhanreigns312@gmail.com>
2024-07-29 23:52:20 +0530

    updated_models

--- scraper/models.py
+++ scraper/models.py
@@ -1,9 +1,21 @@
 from django.db import models
 
+class Genre(models.Model):
+    name=models.CharField(max_length=50)
+    created_on=models.DateTimeField(auto_now_add=True)
+    updated_on=models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
 class Movie(models.Model):
     title = models.CharField(max_length=200,blank=False,null=False)
     director=models.CharField(max_length=100,blank=False,null=False)
     outline = models.TextField(blank=False,null=False)
+    language=models.CharField(max_length=52,null=True,blank=True)
+    released_on=models.CharField(max_length=100,blank=True,null=True)
+    runtime=models.CharField(max_length=24,blank=True,null=True)
+    genre=models.ManyToManyField(Genre,blank=True,null=True)
     movie_url = models.URLField()
     
     def __str__(self):
--- scraper/scrape.py
+++ scraper/scrape.py
@@ -11,15 +11,25 @@ def scrape_and_save_movie(movie_url):
         movie_outline=soup.find('div',class_='synopsis-wrap').find_all('rt-text')[1]
         movie_director=soup.find('section',class_='media-info').find('div',class_='content-wrap').find('dl').find_all('div',class_='category-wrap')[0].find('rt-link')
         
-        # movie_genre=(soup.find('section',class_='media-info').find('div',class_='content-wrap').find('dl').find_all('div',class_='category-wrap')[6]).find('dd')
-        # movie_language=soup.find('section',class_='media-info').find('div',class_='content-wrap').find('dl').find_all('div',class_='category-wrap')[7].find('rt-link')
-        # movie_released_on=soup.find('section',class_='media-info').find('div',class_='content-wrap').find('dl').find_all('div',class_='category-wrap')[8].find('rt-link')
-        # movie_runtime=soup.find('section',class_='media-info').find('div',class_='content-wrap').find('dl').find_all('div',class_='category-wrap')[11].find('rt-link')
-        
+
+        movie_genre=(soup.find('section',class_='media-info').find('div',class_='content-wrap').find('dl').find_all('div',class_='category-wrap')[6]).find('dd')
+        movie_language=(soup.find('section',class_='media-info').find('div',class_='content-wrap').find('dl').find_all('div',class_='category-wrap')[7]).find('dd').find('rt-text')
+        movie_released=(soup.find('section',class_='media-info').find('div',class_='content-wrap').find('dl').find_all('div',class_='category-wrap')[8]).find('dd').find('rt-text')
+        movie_released_date=(movie_released.text).split(',')[0]+(movie_released.text).split(',')[1]
+        movie_runtime=(soup.find('section',class_='media-info').find('div',class_='content-wrap').find('dl').find_all('div',class_='category-wrap')[10]).find('dd').find('rt-text')
+
+        genre_list=[]
+        mg=movie_genre.find_all('rt-link')
+        for i in mg:
+            genre_list.append(i.text)
+
         movie = Movie.objects.create(
         title=movie_title.text,
         director=movie_director.text,
         outline=movie_outline.text,
+        language=movie_language.text,
+        released_on=movie_released_date,
+        runtime=movie_runtime.text,
         movie_url=movie_url,
         )
         return movie
